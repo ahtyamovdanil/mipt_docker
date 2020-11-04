653ffdbc9409551260050cbf065e9b555cca3426
root <root@pop-os.localdomain>
2020-11-04 23:28:57 +0300

    service 3 done

--- mysql_filler.sh
+++
@@ -1,2 +0,0 @@
-#!bin/bash
-mysql -u root -p24812481 -h db -e "load data local infile '$1' into table test_db.potluck fields terminated by ',' enclosed by '"' lines terminated by '\n'"
--- writer.py
+++ writer.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import os
 from tabulate import tabulate
 import csv
 
